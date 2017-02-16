ba0f5dc5cd152a448a339e09150209e37313906a
brianlee <bnlee13@gmail.com>
2017-02-16 00:18:25 -0500

    get live streams

--- twitch.py
+++ twitch.py
@@ -1,27 +1,6 @@
 from config import *
 import requests
 
-# class TwitchStream:
-#     def __init__(self, game='Counter-Strike: Global Offensive'):
-#         self._game = game
-#
-#     @classmethod
-#     def _get(cls, game='', endpoint='', **kwargs):
-#         url = 'https://api.twitch.tv/kraken/' + endpoint
-#         headers = {'Client-ID': twitch_client_id}
-#         params = kwargs
-#         params['game'] = 'Counter-Strike: Global Offensive'
-#         return requests.get(url, headers=headers, params=kwargs)
-#
-#     @classmethod
-#     def get_streams(cls, live='live', limit=10):
-#         res = cls._get(endpoint='streams', live=live, limit=limit).json()
-#         print(res['_total'])
-#         print(len(res))
-
-
-# t = TwitchStream()
-# x = t.get_streams()
 
 class TwitchException(Exception):
     def __init__(self, http_status, code, msg):
@@ -36,54 +15,33 @@ class TwitchException(Exception):
 class Twitch:
     max_get_retries = 10
 
-    def __init__(self, auth=None):
-        '''
-        Create API object.
-        :param auth: An authorization token (optional)
-        '''
+    def __init__(self):
+        ''' Create a Twitch API Object '''
+
         self.prefix = 'https://api.twitch.tv/kraken/'
         self._auth_headers = {'Client-ID': twitch_client_id}
 
-
-    def _get(self, url, args=None, payload=None, **kwargs):
-        if args:
-            kwargs.update(args)
-        kwargs['game'] = 'Counter-Strike: Global Offensive'
-        retries = self.max_get_retries
-        delay = 1
-        url = self.prefix + 'streams'
+    def _get(self, endpoint, **kwargs):
+        url = self.prefix + endpoint
         return requests.get(url, headers=self._auth_headers, params=kwargs)
-        # while retries > 0:
-        #     try:
-        #         return requests.get(url=self.prefix, params=kwargs)
-        #     except TwitchException as e:
-        #         retries -= 1
-        #         status = e.http_status
-        #         # 429 is rate limit
-        #         if status == 429 or (status >= 500 and status < 600):
-        #             if retries < 0:
-        #                 raise
-        #             else:
-        #                 sleep_seconds = int(e.headers.get('Retry-After', delay))
-        #                 print ('retrying ...' + str(sleep_seconds) + 'secs')
-        #                 time.sleep(sleep_seconds + 1)
-        #                 delay += 1
-        #         else:
-        #             raise
-        #     except Exception as e:
-        #         raise
-        #         print ('exception', str(e))
-        #         # some other exception. Requests have
-        #         # been know to throw a BadStatusLine exception
-        #         retries -= 1
-        #         if retries >= 0:
-        #             sleep_seconds = int(e.headers.get('Retry-After', delay))
-        #             print ('retrying ...' + str(delay) + 'secs')
-        #             time.sleep(sleep_seconds + 1)
-        #             delay += 1
-        #         else:
-        #             raise
+
+    def get_channel(self, channel):
+        # for now, i can't get channel by _id.. so just use channel name
+        r = self._get('channels/' + str(channel))
+        return r.json()
+
+    def get_live_streams(self, game=None, **kwargs):
+        r = self._get('streams/', **kwargs).json()
+        streams = r['streams']
+        return [s for s in streams if self._valid_stream(s)]
+
+    def _valid_stream(self, stream):
+        # channel must be live and at least 300 viewers
+        return stream['channel']['status'] and stream['viewers'] >= 300
+        return True
 
 t = Twitch()
-r = t._get('')
-print(r.json())
+# r = t.get_live_streams(game='Counter-Strike: Global Offensive', limit=2)
+r = t.get_channel('dreamhackcs')
+
+print(r)
