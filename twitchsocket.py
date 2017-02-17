328b98014e07d367d9315d607611fa0d118f0bde
brianlee <bnlee13@gmail.com>
2017-02-17 00:07:26 -0500

    cleaned up socket methods. need to import time to calc freq

--- twitchsocket.py
+++ twitchsocket.py
@@ -1,84 +1,64 @@
 from config import twitch_cfg
 import socket
 
-# chat_login is the scope needed for oauth
-
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-# 	s.connect((HOST, PORT))
-# 	s.send(("PASS " + PASS + "\r\n").encode())
-# 	s.send(("NICK " + IDENT + "\r\n").encode())
-# 	s.send(("JOIN #" + CHANNEL + "\r\n").encode())
-# 	data = s.recv(1024)
-
-
-# print('received', repr(data.decode()))
-
-class ChatListener:
-	def __init__(channel):
-		self.channel = channel
-
-	def listen():
-		pass
-
-def openSocket():
-	s = socket.socket()
-	s.connect((twitch_cfg['HOST'], twitch_cfg['PORT']))
-	s.send(("PASS " + PASS + "\r\n").encode('utf-8'))
-	s.send(("NICK " + IDENT + "\r\n").encode('utf-8'))
-	s.send(("JOIN " + CHANNEL + "\r\n").encode('utf-8'))
-	return s
-
-def sendMessage(s, message):
-	messageTemp = "PRIVMSG #" + CHANNEL + " :" + message
-	s.send((messageTemp + "\r\n").encode())
-	print("Sent: " + messageTemp)
-
-def joinRoom(s):
+def join_room(s):
 	readbuffer = ""
-	Loading = True
-	while Loading:
-		readbuffer = readbuffer + str(s.recv(1024), 'utf-8')
+	loading = True
+	while loading:
+		readbuffer += str(s.recv(1024), 'utf-8')
 		temp = readbuffer.split("\n")
 		readbuffer = temp.pop()
 
 		for line in temp:
 			print(line)
-			Loading = loadingComplete(line)
-	sendMessage(s, "Successfully joined chat")
+			# twitch irc's last msg on initial login is the one specified below
+			loading = "End of /NAMES list" in line
 
-def loadingComplete(line):
-	if("End of /NAMES list" in line):
-		return False
-	else:
-		return True
+def send_msg(s, message, channel):
+	msg = "PRIVMSG #" + channel + " :" + message
+	s.send((msg + "\r\n").encode())
 
-def getUser(line):
+# example socket respose
+# :user123!user123@user123.tmi.twitch.tv PRIVMSG #somechannel_tv :nice shot!
+def get_user(line):
 	separate = line.split(":", 2)
 	user = separate[1].split("!", 1)[0]
 	return user
-def getMessage(line):
+
+def get_msg(line):
 	separate = line.split(":", 2)
 	message = separate[2]
 	return message
 
-s = openSocket()
-joinRoom(s)
-readbuffer = ""
+class ChatListener:
+	def __init__(self, channel):
+		self.channel = channel
 
-while True:
-		readbuffer = readbuffer + str(s.recv(1024), 'utf-8')
-		temp = readbuffer.split("\n")
-		readbuffer = temp.pop()
+	def execute(self):
+		"""
+		This function will eventually call a chron to go on twitch and
+		create/dl clips when the 'time is right'
+		"""
 
-		for line in temp:
-			print(line)
-			if "PING" in line:
-				s.send(line.replace("PING", "PONG").encode('utf-8'))
-				break
-			user = getUser(line)
-			message = getMessage(line)
-			print(user + " typed :" + message)
-			if "You Suck" in message:
-				sendMessage(s, "No, you suck!")
-				break
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.connect((twitch_cfg['HOST'], twitch_cfg['PORT']))
+			s.send(("PASS " + twitch_cfg['PASS'] + "\r\n").encode())
+			s.send(("NICK " + twitch_cfg['IDENT'] + "\r\n").encode())
+			s.send(("JOIN " + '#adren_tv' + "\r\n").encode())
+
+			join_room(s)
+			readbuffer = ""
+
+			while True:
+				readbuffer += str(s.recv(1024), 'utf-8')
+				temp = readbuffer.split("\n")
+				readbuffer = temp.pop()
+
+				for line in temp:
+					print(line)
+					if "PING" in line:
+						s.send(line.replace("PING", "PONG").encode('utf-8'))
+						break
+
+c = ChatListener('#adren_tv')
+c.execute()
